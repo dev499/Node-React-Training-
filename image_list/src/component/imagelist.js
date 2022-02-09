fe805d622bb724fe3f57e67aaef7193e891a3512
dev_agarwal <dev@nssplindia.com>
2022-02-09 15:16:06 +0530

    weather app

--- image_list/src/component/imagelist.js
+++ image_list/src/component/imagelist.js
@@ -3,8 +3,13 @@ import React from "react";
 const Imagelist = (props) => {
   console.log(props);
   const Image = props.Image.map((images) => {
-    return <img src={images.webformatURL} alt="images" />;
-  });
+    return (
+      <div>
+        <img src={images.webformatURL} alt="images" />
+        <li key={images.id}>image-{images.id}</li>
+      </div>
+    );
+  });     
   return <div>{Image}</div>;
 };
 
